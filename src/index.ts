9b16504e4510510e4a5c6f2cc21b956f2f5acf2b
Rodrigo Quisbert Mamani <rodrigoquisbert543@gmail.com>
2026-05-21 15:47:05 -0400

    fix: corregir ruta de importación en src/index.ts
    - Cambiar import de ./server/src/index a ../server/src/index
    - La ruta relativa debe salir del directorio src/

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 // Punto de entrada para Render - importa y ejecuta el servidor del subdirectorio server
-import { startServer } from './server/src/index';
+import { startServer } from '../server/src/index';
 
 startServer();
